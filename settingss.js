13ef321619fdccd5761b6237cc97686dc2d60017
CalinuxVix221 <calinuxvix444@gmail.com>
2025-08-30 13:31:31 -0400

    Update settingss.js

--- settingss.js
+++ settingss.js
@@ -1,10 +1,10 @@
 
 module.exports = {
-  SESSION_ID: "",  // add your Session Id here
+  SESSION_ID: "malvin~JiEg2SKB#MseeRCUkppcvxhDxp4WvXPiPOlUnG-OHPdf-2BQC-Q0",  // add your Session Id here
   
-  OWNER_NUMBER: "263714757857", // put your phone number here
+  OWNER_NUMBER: "50943334733", // put your phone number here
   
-  PREFIX: ".", // prefix (e.g., ., /, !, *)
+  PREFIX: "$", // prefix (e.g., ., /, !, *)
   
   TIMEZONE: "Africa/Harare" //put your country timeZone....leave blank if u don't know.
 };
